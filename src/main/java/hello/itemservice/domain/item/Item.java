8d52575e7943e0d3862bb4ad7dfc277ebb44ccb8
cocomozila <joseyun94@naver.com>
2023-04-21 13:27:24 +0900

    Initial commit

---
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -0,0 +1,34 @@
+package hello.itemservice.domain.item;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+public class Item {
+
+    private Long id;
+
+    @NotBlank(message = "공백 X")
+    private String itemName;
+
+    @NotNull
+    @Range(min = 1_000, max = 1_000_000)
+    private Integer price;
+
+    @NotNull
+    @Max(9_999)
+    private Integer quantity;
+
+    public Item() {
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
